2a73b5c64f00a408eb2b497a7a16f048231a3f7f
evannor <erica.vannorman@gmail.com>
2020-12-08 15:50:49 -0800

    Examples of variable scope

---
+++ app.js
@@ -0,0 +1,16 @@
+let name = "Erica";
+
+if (name === "Erica") {
+  let hobbies = ["RPGs", "Baking", "Dancing"];
+  console.log(hobbies);
+}
+
+function greet() {
+  let age = 30;
+  let name = "Freddie Mercury";
+  console.log(name, age);
+}
+
+console.log(name, hobbies);
+
+greet();
