8333c67b9c225b129fae0b2877d54fc057efacc3
evannor <erica.vannorman@gmail.com>
2020-12-16 10:38:09 -0800

    Examples of JS Hoisting

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-let name = "Erica";
+// let name = "Erica";
 
-if (name === "Erica") {
-  let hobbies = ["RPGs", "Baking", "Dancing"];
-  console.log(hobbies);
-}
+// if (name === "Erica") {
+//   let hobbies = ["RPGs", "Baking", "Dancing"];
+//   console.log(hobbies);
+// }
 
-function greet() {
-  let age = 30;
-  let name = "Freddie Mercury";
-  console.log(name, age);
-}
+// function greet() {
+//   let age = 30;
+//   let name = "Freddie Mercury";
+//   console.log(name, age);
+// }
 
-console.log(name, hobbies);
+// console.log(name, hobbies);
 
-greet();
+// greet();
+
+console.log(userName);
+
+let userName = "Erica";
+// With let and const, browswer throws an error when variable is defined after it has been called
+// browser will log 'undefined' only when using var; browser invisiblely pulls up the variable and declares it at the begining of the file HOISTING
